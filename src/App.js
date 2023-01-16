2b6252727b892c4986dc2c4908dd5fd2b9629323
unknown <jcmm1519@gmail.com>
2023-01-16 03:42:21 -0500

    modal

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import './index.css';
+import styled from 'styled-components';
+
+const App = () => {
+	return (
+		<div>
+			<ContenedorBotones>
+				<Boton>Modal 1</Boton>
+			</ContenedorBotones>
+		</div>
+	);
+}
+ 
+export default App;
+
+const ContenedorBotones = styled.div`
+	padding: 40px;
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	gap: 20px;
+`;
+
+const Boton = styled.button`
+	display: block;
+	padding: 10px 30px;
+	border-radius: 100px;
+	color: #fff;
+	border: none;
+	background: #1766DC;
+	cursor: pointer;
+	font-family: 'Roboto', sans-serif;
+	font-weight: 500;
+	transition: .3s ease all;
+
+	&:hover {
+		background: #0066FF;
+	}
+`;
+
+const Contenido = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	h1 {
+		font-size: 42px;
+		font-weight: 700;
+		margin-bottom: 10px;
+	}
+
+	p {
+		font-size: 18px;
+		margin-bottom: 20px;
+	}
+
+	img {
+		width: 100%;
+		vertical-align: top;
+		border-radius: 3px;
+	}
+`;
